bcf04885fa4b99733c4f9669c43d0fff41ea411f
syler <hileo77@gmail.com>
2020-05-03 10:38:48 +0200

    remove comment

--- src/languageFeatures/completion/completion.ts
+++ src/languageFeatures/completion/completion.ts
@@ -4,11 +4,7 @@ import {
   TextDocumentPositionParams,
 } from 'vscode-languageserver';
 
-// This handler provides the initial list of the completion items.
 export const completion = (pos: TextDocumentPositionParams): CompletionItem[] => {
-  // The pass parameter contains the position of the text document in
-  // which code complete got requested. For the example we ignore this
-  // info and always provide the same completion items.
   return [
     {
       label: '"""""TEST AWD"""""',
