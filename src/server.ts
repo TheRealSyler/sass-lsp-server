daae06eefe6a7e0e0496082f3e33bda7813eb8a7
syler <hileo77@gmail.com>
2020-05-03 10:41:49 +0200

    move lsp server to SassLspServer class

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,4 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import {
-  createConnection,
-  ProposedFeatures,
   InitializeParams,
   DidChangeConfigurationNotification,
   TextDocumentSyncKind,
@@ -19,7 +12,7 @@ import {
 import { completion } from './languageFeatures/completion/completion';
 import { AbstractSyntaxTree } from './abstractSyntaxTree/abstractSyntaxTree';
 
-interface ServerSettings {
+export interface LSPServerSettings {
   maxNumberOfProblems: number;
 }
 export type FileSettings = {
@@ -27,15 +20,20 @@ export type FileSettings = {
   insertSpaces: boolean;
 };
 
-class Server {
+export const defaultFileSettings: FileSettings = {
+  insertSpaces: false,
+  tabSize: 2,
+};
+
+export const defaultLSPServerSettings: LSPServerSettings = {
+  maxNumberOfProblems: 1000,
+};
+export class SassLspServer {
   hasConfigurationCapability = false;
   hasWorkspaceFolderCapability = false;
   hasDiagnosticRelatedInformationCapability = false;
 
-  defaultSettings: ServerSettings = { maxNumberOfProblems: 1000 };
-  globalSettings: ServerSettings = this.defaultSettings;
-  // Cache the settings of all open documents
-  documentSettings: Map<string, Thenable<ServerSettings>> = new Map();
+  globalSettings: LSPServerSettings = defaultLSPServerSettings;
 
   ast = new AbstractSyntaxTree();
 
@@ -44,7 +42,13 @@ class Server {
     connection.onInitialized(this.onInitialized);
     connection.onDidChangeConfiguration(this.onDidChangeConfiguration);
     connection.onCompletion(completion);
-    connection.onCompletionResolve((item) => item);
+    connection.onDidOpenTextDocument(async ({ textDocument }) => {
+      console.log('SERVER ON OPEN', textDocument);
+      await this.ast.parseFile(
+        textDocument,
+        await this.getDocumentEditorSettings(textDocument.uri)
+      );
+    });
 
     connection.listen();
   }
@@ -63,9 +67,8 @@ class Server {
     const result: InitializeResult = {
       capabilities: {
         textDocumentSync: TextDocumentSyncKind.Incremental,
-        // Tell the client that the server supports code completion
         completionProvider: {
-          resolveProvider: true,
+          resolveProvider: false,
         },
       },
     };
@@ -94,17 +97,9 @@ class Server {
   private onDidChangeConfiguration: NotificationHandler<DidChangeConfigurationParams> = (
     change
   ) => {
-    if (this.hasConfigurationCapability) {
-      // Reset all cached document settings
-      this.documentSettings.clear();
-    } else {
-      this.globalSettings = <ServerSettings>(
-        (change.settings.languageServerExample || this.defaultSettings)
-      );
-    }
-
-    // Revalidate all open text documents
-    // documents.all().forEach(validateTextDocument);
+    this.globalSettings = <LSPServerSettings>(
+      (change.settings.languageServerExample || defaultLSPServerSettings)
+    );
   };
 
   private getHasWorkspaceFolderCapability(capabilities: ClientCapabilities): boolean {
@@ -122,200 +117,19 @@ class Server {
       capabilities.textDocument.publishDiagnostics.relatedInformation
     );
   }
+
+  private async getDocumentEditorSettings(uri: string): Promise<FileSettings> {
+    console.log('SERVER GET CON', uri);
+    if (!this.hasConfigurationCapability) {
+      return defaultFileSettings;
+    }
+
+    return {
+      ...defaultFileSettings,
+      ...(await this.connection.workspace.getConfiguration({
+        scopeUri: uri,
+        section: 'editor',
+      })),
+    };
+  }
 }
-
-const server = new Server(createConnection(ProposedFeatures.all));
-
-// // Create a connection for the server. The connection uses Node's IPC as a transport.
-// // Also include all preview / proposed LSP features.
-// let connection = createConnection(ProposedFeatures.all);
-
-// // Create a simple text document manager. The text document manager
-// // supports full document sync only
-// let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
-
-// let hasConfigurationCapability: boolean = false;
-// let hasWorkspaceFolderCapability: boolean = false;
-// let hasDiagnosticRelatedInformationCapability: boolean = false;
-
-// connection.onInitialize((params: InitializeParams) => {
-//   let capabilities = params.capabilities;
-
-//   // Does the client support the `workspace/configuration` request?
-//   // If not, we will fall back using global settings
-//   hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration);
-//   hasWorkspaceFolderCapability = !!(
-//     capabilities.workspace && !!capabilities.workspace.workspaceFolders
-//   );
-//   hasDiagnosticRelatedInformationCapability = !!(
-//     capabilities.textDocument &&
-//     capabilities.textDocument.publishDiagnostics &&
-//     capabilities.textDocument.publishDiagnostics.relatedInformation
-//   );
-
-//   const result: InitializeResult = {
-//     capabilities: {
-//       textDocumentSync: TextDocumentSyncKind.Incremental,
-//       // Tell the client that the server supports code completion
-//       completionProvider: {
-//         resolveProvider: true,
-//       },
-//     },
-//   };
-//   if (hasWorkspaceFolderCapability) {
-//     result.capabilities.workspace = {
-//       workspaceFolders: {
-//         supported: true,
-//       },
-//     };
-//   }
-//   return result;
-// });
-
-// connection.onInitialized(() => {
-//   if (hasConfigurationCapability) {
-//     // Register for all configuration changes.
-//     connection.client.register(DidChangeConfigurationNotification.type, undefined);
-//   }
-//   if (hasWorkspaceFolderCapability) {
-//     connection.workspace.onDidChangeWorkspaceFolders((_event) => {
-//       connection.console.log('Workspace folder change event received.');
-//     });
-//   }
-// });
-
-// // The example settings
-// interface ExampleSettings {
-//   maxNumberOfProblems: number;
-// }
-
-// // The global settings, used when the `workspace/configuration` request is not supported by the client.
-// // Please note that this is not the case when using this server with the client provided in this example
-// // but could happen with other clients.
-// const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-// let globalSettings: ExampleSettings = defaultSettings;
-
-// const ast = new AbstractSyntaxTree();
-
-// // Cache the settings of all open documents
-// let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
-
-// connection.onDidChangeConfiguration((change) => {
-//   if (hasConfigurationCapability) {
-//     // Reset all cached document settings
-//     documentSettings.clear();
-//   } else {
-//     globalSettings = <ExampleSettings>(change.settings.languageServerExample || defaultSettings);
-//   }
-
-//   // Revalidate all open text documents
-//   documents.all().forEach(validateTextDocument);
-// });
-
-// function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-//   if (!hasConfigurationCapability) {
-//     return Promise.resolve(globalSettings);
-//   }
-//   let result = documentSettings.get(resource);
-//   if (!result) {
-//     result = connection.workspace.getConfiguration({
-//       scopeUri: resource,
-//       section: 'languageServerExample',
-//     });
-//     documentSettings.set(resource, result);
-//   }
-//   return result;
-// }
-
-// // Only keep settings for open documents
-// documents.onDidClose((e) => {
-//   documentSettings.delete(e.document.uri);
-// });
-
-// // The content of a text document has changed. This event is emitted
-// // when the text document first opened or when its content has changed.
-// documents.onDidChangeContent((change) => {
-//   validateTextDocument(change.document);
-// });
-
-// async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-//   // In this simple example we get the settings for every validate run.
-//   let settings = await getDocumentSettings(textDocument.uri);
-
-//   // The validator creates diagnostics for all uppercase words length 2 and more
-//   let text = textDocument.getText();
-//   let pattern = /\b[A-Z]{2,}\b/g;
-//   let m: RegExpExecArray | null;
-
-//   let problems = 0;
-//   let diagnostics: Diagnostic[] = [];
-//   while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-//     problems++;
-//     let diagnostic: Diagnostic = {
-//       severity: DiagnosticSeverity.Warning,
-//       range: {
-//         start: textDocument.positionAt(m.index),
-//         end: textDocument.positionAt(m.index + m[0].length),
-//       },
-//       message: `${m[0]} is all uppercase.`,
-//       source: 'ex',
-//     };
-//     if (hasDiagnosticRelatedInformationCapability) {
-//       diagnostic.relatedInformation = [
-//         {
-//           location: {
-//             uri: textDocument.uri,
-//             range: Object.assign({}, diagnostic.range),
-//           },
-//           message: 'Spelling matters',
-//         },
-//         {
-//           location: {
-//             uri: textDocument.uri,
-//             range: Object.assign({}, diagnostic.range),
-//           },
-//           message: 'Particularly for names',
-//         },
-//       ];
-//     }
-//     diagnostics.push(diagnostic);
-//   }
-
-//   // Send the computed diagnostics to VSCode.
-//   connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-// }
-
-// connection.onDidChangeWatchedFiles((_change) => {
-//   // Monitored files have change in VSCode
-//   connection.console.log('We received an file change event');
-// });
-
-// connection.onCompletion(completion);
-// connection.onCompletionResolve((item) => item);
-
-/*
-connection.onDidOpenTextDocument((params) => {
-	// A text document got opened in VSCode.
-	// params.textDocument.uri uniquely identifies the document. For documents store on disk this is a file URI.
-	// params.textDocument.text the initial full content of the document.
-	connection.console.log(`${params.textDocument.uri} opened.`);
-});
-connection.onDidChangeTextDocument((params) => {
-	// The content of a text document did change in VSCode.
-	// params.textDocument.uri uniquely identifies the document.
-	// params.contentChanges describe the content changes to the document.
-	connection.console.log(`${params.textDocument.uri} changed: ${JSON.stringify(params.contentChanges)}`);
-});
-connection.onDidCloseTextDocument((params) => {
-	// A text document got closed in VSCode.
-	// params.textDocument.uri uniquely identifies the document.
-	connection.console.log(`${params.textDocument.uri} closed.`);
-});
-*/
-
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-// documents.listen(connection);
-
-// Listen on the connection
-// connection.listen();
